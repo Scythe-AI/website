6b6ef3a5f34a6eeba6d03fca706954632d60c659
Ethan Yu <ethayu1@gmail.com>
2023-04-06 16:53:42 -0400

    create icons

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Dropdown from '../utils/Dropdown';
-import logo from '../images/logo.png';
+// import logo from '../images/logo.png';
 
 function Header() {
 
