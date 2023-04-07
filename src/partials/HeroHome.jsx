8c1615313eb67887504ee37ceed5ad6401a1e82a
Ethan Yu <ethayu1@gmail.com>
2023-04-07 01:13:31 -0400

    change button naming

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -45,7 +45,7 @@ function HeroHome() {
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
                 <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#form">
-                  Start free trial
+                  Get in Touch
                 </a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
